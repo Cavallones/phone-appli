8b76a49274735a592ba26b618debe4e43b909ba5
Cavallones <q.sofian@gmail.com>
2017-03-11 02:22:11 +0100

    Works on device without using Android Studio

--- src/main/java/org/project/MainActivity.java
+++ src/main/java/org/project/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Phone extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
